71561f6eaf0fb5169703c72590dce73dbe038d80
Abigail Watson <awatson1978@gmail.com>
2015-08-18 22:37:33 -0500

    initial commit

---
+++ tests/nightwatch/commands/api/meteor/checkSession.js
@@ -0,0 +1,17 @@
+
+
+
+// syncrhonous version; only works for checking javascript objects on client
+exports.command = function (sessionVarName, expectedValue) {
+  var client = this;
+  this
+    .execute(function (data) {
+      return Session.get(data);
+    }, [sessionVarName], function (result) {
+      client.assert.ok(result.value);
+      if (expectedValue) {
+        client.assert.equal(result.value, expectedValue);
+      }
+    }).pause(1000);
+  return this;
+};
